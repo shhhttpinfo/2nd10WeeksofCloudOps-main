20f52d2762c6102a1a66030857773efc0df18820
shhhttpinfo <syedmudassir484sd@gmail.com>
2026-01-09 21:22:54 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://a033efa1e8b794dbbabf1603dfc2634f-1968123503.us-west-2.elb.amazonaws.com"
+const API_BASE_URL = "http://a1d08b794fc4149b5bb873ca08047702-612719776.us-east-1.elb.amazonaws.com"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
